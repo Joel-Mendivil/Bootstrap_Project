8b5e7eb8e1d1674aad7c31c8e864d554941e384f
David L <dlopez222@west-mec.org>
2020-10-27 15:06:45 -0700

    added a api to hearthstone

--- hearthstone.js
+++ hearthstone.js
@@ -1,29 +1,30 @@
 ////////////////Use Dom to select get and post button/////////////
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
+
 ///////create callback function thats used when button clicked////////
 const getData = () =>{
-    var answer = document.getElementById('oName').value;
-    var answer = document.getElementById('oPlatform').value;
-    console.log(answer);
-    var tops = "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/{gamertag}/{platform}" + answer;
+    var tops = "omgvamp-hearthstone-v1.p.rapidapi.com";
     sendHttpRequest('GET', tops);
 };
+
+
 const sendHttpRequest = (method, url) => {
     const promise = new Promise((resolve, reject) =>{
     console.log(url);
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/{gamertag}/{platform}");
-    xhr.setRequestHeader("x-rapidapi-host", "call-of-duty-modern-warfare.p.rapidapi.com");
+    xhr.open("GET", "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/");
+    xhr.setRequestHeader("x-rapidapi-host", "omgvamp-hearthstone-v1.p.rapidapi.com");
     xhr.setRequestHeader("x-rapidapi-key", "3180031b14mshd2c34497d5a9991p182be6jsnf5da9a27785b");
     xhr.onload = () => {
 ///////////// When browser load parse http req to usable format//////////
             const data = JSON.parse(xhr.response);//converts JSON data to RAW JS Objs & dataTypes
             console.log(data)//JSON Data
 //////// Assign diferent parts to pokedex using dom selection/////////
-            document.getElementById('info_wins').value = data[0].player.wins;
-            document.getElementById('info_kd').value = data[0].player.kd;
-            document.getElementById('info_time').value = data[0].player.time;
+            document.getElementById('info_name').value = data.Classic[200].name;
+            document.getElementById('info_id').value = data.Classic[200].cardId;
+            document.getElementById('info_type').value = data.Classic[200].type;
+            document.getElementById('info_health').value = data.Classic[200].health;
         }
         //or xhr.addeventlistener('load', function)
         xhr.send();
